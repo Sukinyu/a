1b1a05f1e3732e43e85e5f693997c157108353a5
Sukinyu <89321435+Sukinyu@users.noreply.github.com>
2025-08-25 19:18:05 -0400

    Update b.js

--- b.js
+++ b.js
@@ -20,7 +20,7 @@ async function injectCaptionsFromPlayer() {
   const trackInfo = player.captions.playerCaptionsTracklistRenderer.captionTracks[0];
   const video = document.querySelector("video");
   if (!video || !trackInfo) return;
-
+/*
   const url = trackInfo.baseUrl + "&pot=Mlq3f81rUkG0Brc8Sb3X66CD2b_5LFvcmbyWCdAU62f4YnDZTWY1-tBntmhO4sV5mDQSUV5clQlRYexOnxDLClUPpDxoyldvLgyi_Elmy5m6wy5a6Qg_lVbzK6M%3D" + "&c=MWEB" + "&fmt=json3"+"&tlang=en";
    json3 = await fetch(url)
     const data = await json3.json();
@@ -41,12 +41,12 @@ alert(data);
 
         const oldTrack = video.querySelector('track[data-injected="true"]');
         if (oldTrack) oldTrack.remove();
-
+*/
         const track = document.createElement("track");
         track.kind = "captions";
         track.label = "Custom CC";
         track.srclang = "en";
-        track.src = URL.createObjectURL(new Blob([vtt], { type: "text/vtt" }));
+       // track.src = URL.createObjectURL(new Blob([vtt], { type: "text/vtt" }));
         track.default = true;
         track.dataset.injected = "true";
         video.appendChild(track);
