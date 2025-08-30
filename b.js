76720fad609db92fa32c94015e56a9e13f031beb
Sukinyu <89321435+Sukinyu@users.noreply.github.com>
2025-08-29 20:29:17 -0400

    Delete b.js

--- b.js
+++
@@ -1,7 +0,0 @@
-new PerformanceObserver(list => {
-  for (const entry of list.getEntries()) {
-    if (entry.name.includes("timedtext") || entry.name.includes("texttrack")) {
-      alert("Caption request: " entry.name);
-    }
-  }
-}).observe({ entryTypes: ["resource"] });
